6f26f73f33bad79e51a701f4b01f954eb075cffa
Sergii Gordiienko <serjgord@gmail.com>
2024-10-24 12:47:32 +0300

    Add basic  Player buttons implementation

---
+++ BookPlayer/BookPlayer/Views/Chapter/Player/PlayerButton.swift
@@ -0,0 +1,102 @@
+//
+//  PlayerButton.swift
+//  BookPlayer
+//
+//  Created by Sergii Gordiienko on 24.10.2024.
+//
+
+import SwiftUI
+
+struct PlayerButton: View {
+    
+    enum Kind {
+        case next
+        case previous
+        case play
+        case pause
+        case forward
+        case backward
+    }
+
+    let kind: Kind
+
+    var body: some View {
+        Button {
+            // TODO: action
+        } label: {
+            
+            Label(kind.labelText, systemImage: kind.systemName)
+                .labelStyle(.iconOnly)
+                .imageScale(kind.scale)
+                
+//            Label { Text("Previous chapter") }
+//            icon: {
+//                Image(systemName: )
+//                    .resizable()
+//                    .scaledToFit()
+//                    .controlSize(.large)
+//            }
+//            .labelStyle(.iconOnly)
+        }
+        .frame(width:  44, height: 44)
+        .foregroundStyle(.black)
+    }
+}
+
+extension PlayerButton.Kind {
+    var systemName: String {
+        switch self {
+        case .next:
+            "forward.end.fill"
+        case .previous:
+            "backward.end.fill"
+        case .play:
+            "play.fill"
+        case .pause:
+            "pause.fill"
+        case .forward:
+            "goforward.10"
+        case .backward:
+            "gobackward.5"
+        }
+    }
+    
+    var labelText: String {
+        switch self {
+        case .next:
+            "Next chapter"
+        case .previous:
+            "Previous chapter"
+        case .play:
+            "Play"
+        case .pause:
+            "Pause"
+        case .forward:
+            "Go forward"
+        case .backward:
+            "Go backward"
+        }
+    }
+    
+    var scale: Image.Scale {
+        switch self {
+        case .next, .previous:
+                .medium
+        case .play, .pause:
+                .large
+        case  .backward, .forward:
+                .medium
+        }
+    }
+}
+
+#Preview {
+    Group {
+        PlayerButton(kind: .previous)
+        PlayerButton(kind: .backward)
+        PlayerButton(kind: .play)
+        PlayerButton(kind: .pause)
+        PlayerButton(kind: .forward)
+        PlayerButton(kind: .next)
+    }
+}
---
+++ BookPlayer/BookPlayer/Views/Chapter/Player/PlayerControlView.swift
@@ -0,0 +1,29 @@
+//
+//  PlayerControlView.swift
+//  BookPlayer
+//
+//  Created by Sergii Gordiienko on 24.10.2024.
+//
+
+import SwiftUI
+
+struct PlayerControlView: View {
+    var body: some View {
+        HStack(alignment: .center) {
+            PlayerButton(kind: .previous)
+            Spacer(minLength: 10)
+            PlayerButton(kind: .backward)
+            Spacer(minLength: 10)
+            PlayerButton(kind: .play)
+            Spacer(minLength: 10)
+            PlayerButton(kind: .forward)
+            Spacer(minLength: 10)
+            PlayerButton(kind: .next)
+        }
+        .padding(.horizontal, 40)
+    }
+}
+
+#Preview {
+    PlayerControlView()
+}
